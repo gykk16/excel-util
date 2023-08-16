6c16d9dc27bca9015e2691ce18bcefef7435ada2
ykk <gykk16@gmail.com>
2023-08-16 20:11:16 +0900

    [REFACTOR]

--- src/main/java/glory/tools/excelutil/reader/ExcelReader.java
+++ src/main/java/glory/tools/excelutil/reader/ExcelReader.java
@@ -1,8 +1,6 @@
 package glory.tools.excelutil.reader;
 
 
-import static glory.tools.excelutil.common.LogStringUtil.logSubTitle;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,148 +10,114 @@ import java.util.Map;
 import java.util.function.ToIntFunction;
 
 import glory.tools.excelutil.exception.ExcelException;
+import org.apache.poi.ss.SpreadsheetVersion;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.util.StopWatch;
 
 public class ExcelReader {
 
-    private static final Logger log = LoggerFactory.getLogger(ExcelReader.class);
-
-    private static final int LIMIT_ROW = 100_100;
+    private static final int LIMIT_ROW = SpreadsheetVersion.EXCEL2007.getMaxRows() - 1_000;
 
     private ExcelReader() {
     }
 
     public static Map<Integer, List<String>> readExcel(String fileLocation, boolean hasHeader) {
-        StopWatch stopWatch = new StopWatch();
-        Map<Integer, List<String>> map = readExcel(fileLocation, hasHeader, stopWatch);
-        log.debug("==> {}", stopWatch);
-        return map;
-    }
-
-    public static Map<Integer, List<String>> readExcel(String fileLocation, boolean hasHeader, StopWatch stopWatch) {
-        log.debug(logSubTitle("readExcel START"));
-        stopWatch.start("read excel");
-
-        Map<Integer, List<String>> data = new HashMap<>();
 
         try (Workbook workbook = new XSSFWorkbook(new FileInputStream(fileLocation))) {
             Sheet sheet = workbook.getSheetAt(0);
+            validateRowLimit(sheet);
 
-            int lastRowNum = sheet.getLastRowNum();
-            if (lastRowNum > LIMIT_ROW) {
-                throw new ExcelException("Excel max row is " + LIMIT_ROW + " row.");
-            }
+            var lastRowNum = sheet.getLastRowNum();
+            var lastCellNum = sheet.getRow(0).getLastCellNum();
+            var startRow = hasHeader ? 1 : 0;
 
-            short lastCellNum = sheet.getRow(0).getLastCellNum();
-            log.debug("# ==> lastCellNum = {}", lastCellNum);
-            log.debug("# ==> lastRowNum = {}", lastRowNum);
+            Map<Integer, List<String>> data = new HashMap<>();
 
-            int startRow = hasHeader ? 1 : 0;
             for (int currentRowNum = startRow; currentRowNum < lastRowNum + 1; currentRowNum++) {
                 data.put(currentRowNum, new ArrayList<>());
-
                 Row row = sheet.getRow(currentRowNum);
                 rowToMap(data, lastCellNum, currentRowNum, row);
             }
 
+            return data;
+
         } catch (IOException e) {
             throw new ExcelException("Excel processing error.", e);
 
-        } finally {
-            stopWatch.stop();
-            log.debug(logSubTitle("readExcel END"));
         }
 
-        return data;
     }
 
-    public static int readExcelAndFunction(String fileLocation, boolean hasHeader, StopWatch stopWatch,
-            int functionBatchSize, ToIntFunction<Map<Integer, List<String>>> function) {
-        log.debug(logSubTitle("readExcelAndFunction START"));
-        stopWatch.start("read excel and function");
+    public static int readExcelAndApplyFunction(String fileLocation, boolean hasHeader, int functionBatchSize,
+            ToIntFunction<Map<Integer, List<String>>> function) {
 
-        int result;
+        int processed = 0;
         try (Workbook workbook = new XSSFWorkbook(new FileInputStream(fileLocation))) {
             Sheet sheet = workbook.getSheetAt(0);
+            validateRowLimit(sheet);
 
-            int lastRowNum = sheet.getLastRowNum();
-            if (lastRowNum > LIMIT_ROW) {
-                throw new ExcelException("Excel max row is " + LIMIT_ROW + " row.");
-            }
+            var lastRowNum = sheet.getLastRowNum();
+            var lastCellNum = sheet.getRow(0).getLastCellNum();
+            var startRow = hasHeader ? 1 : 0;
 
-            short lastCellNum = sheet.getRow(0).getLastCellNum();
-            log.debug("# ==> lastCellNum = {}", lastCellNum);
-            log.debug("# ==> lastRowNum = {}", lastRowNum);
-
-            int processed = 0;
-            int startRow = hasHeader ? 1 : 0;
             Map<Integer, List<String>> data = new HashMap<>();
 
             for (int currentRowNum = startRow; currentRowNum < lastRowNum + 1; currentRowNum++) {
 
                 if (currentRowNum != 0 && currentRowNum % functionBatchSize == 0) {
-                    // Apply function
+                    /* Apply function */
                     processed += function.applyAsInt(data);
-                    // clear Map
                     data.clear();
                 }
 
-                // create row in Map
                 data.put(currentRowNum, new ArrayList<>());
-
-                // save row data into Map
                 Row row = sheet.getRow(currentRowNum);
                 rowToMap(data, lastCellNum, currentRowNum, row);
             }
 
-            // Apply function
+            /* Apply function */
             processed += function.applyAsInt(data);
-            // clear Map
             data.clear();
 
-            result = processed;
+            return processed;
 
         } catch (IOException e) {
             throw new ExcelException("Excel processing error.", e);
 
-        } finally {
-            stopWatch.stop();
-            log.debug(logSubTitle("readExcelAndFunction END"));
         }
+    }
 
-        return result;
+    private static void validateRowLimit(Sheet sheet) {
+        if (sheet.getLastRowNum() > LIMIT_ROW) {
+            throw new ExcelException("Excel max row is " + LIMIT_ROW + " row.");
+        }
     }
 
     private static void rowToMap(Map<Integer, List<String>> data, short lastCellNum, int currentRowNum, Row row) {
         for (int currCellNum = 0; currCellNum < lastCellNum; currCellNum++) {
             Cell cell = row.getCell(currCellNum);
-
-            if (cell == null) {
-                data.get(currentRowNum).add("");
-                continue;
-            }
-
-            switch (cell.getCellType()) {
-                case STRING -> data.get(currentRowNum).add(cell.getRichStringCellValue().getString().trim());
-                case NUMERIC -> {
-                    if (DateUtil.isCellDateFormatted(cell)) {
-                        data.get(currentRowNum).add(cell.getLocalDateTimeCellValue() + "");
-                    } else {
-                        data.get(currentRowNum).add((long)cell.getNumericCellValue() + "");
-                    }
-                }
-                case BOOLEAN -> data.get(currentRowNum).add(cell.getBooleanCellValue() + "");
-                case FORMULA -> data.get(currentRowNum).add(cell.getCellFormula() + "");
-                default -> data.get(currentRowNum).add("");
-            }
+            String value = extractCellValue(cell);
+            data.get(currentRowNum).add(value);
         }
+    }
+
+    private static String extractCellValue(Cell cell) {
+        if (cell == null) {
+            return "";
+        }
+
+        return switch (cell.getCellType()) {
+            case STRING -> cell.getRichStringCellValue().getString().trim();
+            case NUMERIC -> DateUtil.isCellDateFormatted(cell)
+                    ? cell.getLocalDateTimeCellValue().toString()
+                    : String.valueOf((long)cell.getNumericCellValue());
+            case BOOLEAN -> String.valueOf(cell.getBooleanCellValue());
+            case FORMULA -> cell.getCellFormula();
+            default -> "";
+        };
     }
 }
